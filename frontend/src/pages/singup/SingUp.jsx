e9e5b31b4381b342bae50ebf5442b0cf869a865b
Anshul <a20110100680@gmail.com>
2024-05-18 16:48:32 +0530

    Singup & Login page done

--- frontend/src/pages/singup/SingUp.jsx
+++ frontend/src/pages/singup/SingUp.jsx
@@ -1,11 +1,94 @@
 import React from 'react'
+import GenderCheckBox from './GenderCheckBox';
 
 const SingUp = () => {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center justify-center min-w-96 max-auto'>
+    <div className='w-full p-6 rounded-lg shadow-md bg-grey-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+      <h1 className='text-3xl font-semibold text-center text-black'> Singup 
+     <span className='text-blue-500 ml-4'>ChatApp</span>
+      </h1>
+
+      {/* FORM, USER SingUp */}
+
+      <form>
+      <div>
+          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Full Name</span> </label>
+          <input type="text" placeholder='Enter Your Full Name' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Username </span> </label>
+          <input type="text" placeholder='Enter username' className='w-full input input-bordered h-10' />
+        </div>
+
+        <div>
+          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Password</span> </label>
+          <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-10' />
+        </div>
+
+        <GenderCheckBox/>
+
+        <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+          Already have an account?
+        </a>
+
+        <div className='btn btn-block btn-sm mt-2'>Sing Up</div>
+
+      </form>
+
     </div>
+</div>
   )
 }
 
-export default Singup
+export default SingUp;
+
+
+//STATER CODE FOR SINGUP 
+
+// import React from 'react'
+//import GenderCheckBox from './GenderCheckBox';
+//
+//const SingUp = () => {
+//  return (
+//    <div className='flex flex-col items-center justify-center min-w-96 max-auto'>
+//    <div className='w-full p-6 rounded-lg shadow-md bg-grey-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
+//      <h1 className='text-3xl font-semibold text-center text-black'> Singup 
+//     <span className='text-blue-500 ml-4'>ChatApp</span>
+//      </h1>
+//
+//      {/* FORM, USER SingUp */}
+//
+//      <form>
+//      <div>
+//          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Full Name</span> </label>
+//          <input type="text" placeholder='Enter Your Full Name' className='w-full input input-bordered h-10' />
+//        </div>
+//
+//        <div>
+//          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Username </span> </label>
+//          <input type="text" placeholder='Enter username' className='w-full input input-bordered h-10' />
+//        </div>
+//
+//        <div>
+//          <label className='label p-2'> <span className='text-base label-text text-white mt-3'> Password</span> </label>
+//          <input type="password" placeholder='Enter Password' className='w-full input input-bordered h-10' />
+//        </div>
+//
+//        <GenderCheckBox/>
+//
+//        <a href='#' className='text-sm hover:underline mt-2 hover:text-blue-600 mt-2 inline-block'>
+//          Already have an account?
+//        </a>
+//
+//        <div className='btn btn-block btn-sm mt-2'>Sing Up</div>
+//
+//      </form>
+//
+//    </div>
+//</div>
+//  )
+//}
+//
+// export default SingUp;
