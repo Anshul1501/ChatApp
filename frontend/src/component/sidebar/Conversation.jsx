18a27dba69cce70916eb914fc1f92866c3455c1f
Anshul <a20110100680@gmail.com>
2024-05-18 19:24:55 +0530

    frontend Static: sidebar & messages done

--- frontend/src/component/sidebar/Conversation.jsx
+++ frontend/src/component/sidebar/Conversation.jsx
@@ -4,7 +4,7 @@ const Conversation = () => {
   return (
     <>
             <div className='flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
-                <div className='avtar online'>
+                <div className='avatar online'>
                     <div className='w-12 rounded-full'>
                         <img src="https://avatar.iran.liara.run/public" alt="user avtar"/>
                     </div>
