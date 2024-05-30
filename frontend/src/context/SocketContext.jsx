6b98e30ab945db09d51ad03cd7a728626636652e
Anshul <a20110100680@gmail.com>
2024-05-30 13:06:32 +0530

    deployement

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const newSocket = io("http://localhost:5000", { // Ensure the port matches your backend server
+            const newSocket = io("https://chatapp-production-7sv0.onrender.com", { // Ensure the port matches your backend server
                 query: {
                     userId: authUser.userId,
                 },
