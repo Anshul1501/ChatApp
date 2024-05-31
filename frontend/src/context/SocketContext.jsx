432f890a8d3d712567e57bc21f1a7ed8e3a59ace
Anshul <a20110100680@gmail.com>
2024-05-31 12:19:05 +0530

    production URI added to socket context

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const newSocket = io("http://localhost:5000", { 
+            const newSocket = io("https://chatapp-production-7sv0.onrender.com/", { 
                 query: {
                     userId: authUser.userId,
                 },
